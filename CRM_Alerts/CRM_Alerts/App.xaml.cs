a2c2164d76f4c9c6e0895e971a39413a324dbf1a
Oscar Emiliano Gallardo Blancas <ogallardo@airam.com.mx>
2019-02-08 11:53:12 -0600

    Prueba 2: Login creado, no hay logica en proyecto por el momento.

---
+++ CRM_Alerts/CRM_Alerts/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+namespace CRM_Alerts
+{
+    using CRM_Alerts.Views;
+    using Xamarin.Forms;
+    public partial class App : Application
+    {
+        #region Constructores
+        public App()
+        {
+            InitializeComponent();
+
+                MainPage = new NavigationPage(new LoginPage());
+        }
+        #endregion
+
+        #region Metodos
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+        #endregion
+    }
+}
---
+++ CRM_Alerts/CRM_Alerts/Infraestructure/InstanceLocator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRM_Alerts.Infraestructure
+{
+    using CRM_Alerts.ModelViews;
+    public class InstanceLocator
+    {
+        #region Propiedades
+       
+        public MainViewModel Main { get; set; }
+        #endregion
+
+        #region Contructores
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
